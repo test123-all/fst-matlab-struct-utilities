5df40873e5454ccc60b44a2a67e655edcc3e909b
Sebastian Neumeier <sebastian.neumeier@mail.de>
2025-01-16 02:00:25 +0100

    Refactor th tests

--- tests/test_convertObjectstructToStruct.m
+++ tests/test_convertObjectstructToStruct.m
@@ -4,6 +4,8 @@ clear
 % TODO: the assert Messages are still wrong.
 % TODO: Maybe add a second functional test where deeper substructs get
 % tested
+% ##### TODO: FIXME: This tests need some classes from the hydropusler test
+% stand software to function properly.
 %% Test 1; Normal functional test
 unrecordableConstantImportantParameters_objectstruct.unrecordable_fuelldruecke.fueldruck1 = unrecordableImportantParameterClass(5, '/filling_pressure_1', 'Pressure', 'bar');
 unrecordableConstantImportantParameters_objectstruct.unrecordable_fuelldruecke.fueldruck2 = unrecordableImportantParameterClass(6, '/filling_pressure_2', 'Pressure', 'bar');
